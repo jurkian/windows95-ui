74be2f60f15ac148008152fb9127a392742f0198
Jakub Jurkian <jakub.jurkian@gmail.com>
2025-10-30 18:07:29 +0100

    Add ProgressBar with solid and striped variants

---
+++ src/components/ProgressBar/ProgressBar.tsx
@@ -0,0 +1,25 @@
+export function Win95ProgressBar({ type = "solid" }) {
+  return (
+    <div className="w-66">
+      <div className="overflow-hidden rounded-none bg-win-grey">
+        {type === "solid" && (
+          <div
+            style={{ width: "37.5%" }}
+            className="h-2 rounded-none bg-win-blue"
+          />
+        )}
+        {type === "striped" && (
+          <div
+            style={{
+              width: "37.5%",
+              backgroundImage:
+                "repeating-linear-gradient(90deg, var(--color-win-blue) 0 10px, transparent 10px 14px)",
+              backgroundSize: "14px 100%",
+            }}
+            className="h-2 rounded-none"
+          />
+        )}
+      </div>
+    </div>
+  );
+}
