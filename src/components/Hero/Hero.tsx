d2e5dfb9150d17aa950e22d0add272f7c21af7f3
Jakub Jurkian <jakub.jurkian@gmail.com>
2025-10-30 19:07:00 +0100

    Add Hero section

---
+++ src/components/Hero/Hero.tsx
@@ -0,0 +1,25 @@
+import { Win95Button } from "../Form/Button/Button";
+
+export function Hero() {
+  return (
+    <div className="bg-[#c8c8c8] win-bevel-raised">
+      <div className="mx-auto max-w-2xl px-6 py-24 sm:py-32 lg:px-8 text-center">
+        <h2 className="text-4xl font-semibold text-gray-900 sm:text-5xl">
+          Boost your productivity. Start using our app today.
+        </h2>
+        <div className="mt-10 flex items-center justify-center gap-x-6">
+          <Win95Button>Help</Win95Button>
+          <a
+            href="#"
+            className="bg-win-blue px-3 py-1.5 text-sm font-semibold text-white shadow-xs"
+          >
+            Get started
+          </a>
+          <a href="#" className="text-sm/6 font-semibold text-gray-900">
+            Learn more <span aria-hidden="true">→</span>
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+}
