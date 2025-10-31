5d79a857c416d61d451fb3e7b90a5c94eba4961a
Jakub Jurkian <jakub.jurkian@gmail.com>
2025-10-31 11:38:47 +0100

    Some RWD improvements

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -3,11 +3,11 @@ import { Win95Button } from "../Form/Button/Button";
 export function Hero() {
   return (
     <div className="bg-[#c8c8c8] win-bevel-raised">
-      <div className="mx-auto max-w-2xl px-6 py-24 sm:py-32 lg:px-8 text-center">
-        <h2 className="text-4xl font-semibold text-gray-900 sm:text-5xl">
+      <div className="mx-auto max-w-2xl px-6 py-14 sm:py-32 lg:px-8 text-center">
+        <h2 className="text-3xl font-semibold text-gray-900 sm:text-5xl">
           Boost your productivity. Start using our app today.
         </h2>
-        <div className="mt-10 flex items-center justify-center gap-x-6">
+        <div className="mt-10 flex items-center justify-center flex-col gap-y-4 sm:flex-row sm:gap-x-6">
           <Win95Button>Help</Win95Button>
           <a
             href="#"
